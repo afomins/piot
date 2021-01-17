c2782f6847f76d1c8bfc3e0ccec30f6ea8640874
Alex Fomins <alex.fomins@ui.com>
2021-01-17 22:17:18 +0200

    wip

--- scripts/piot2-wrapper.sh
+++ scripts/piot2-wrapper.sh
@@ -4,6 +4,7 @@ PIOT="/home/afomins/git/piot/scripts/piot2.py"
 SENSOR_ID="00000000000"
 SENSOR_NAME="br5-bsmt-temp-heater-in"
 
+
 while [ 1 ]; do
     out=`$PIOT --action=read-sensor-ds18b20 --sensor-id=$SENSOR_ID --random`
     [ x`echo $out | jq .success` != xtrue ] && break
