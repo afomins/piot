719c64dae09ec1f63ee9ed70e776a29bf0df3070
Alex Fomins <afomins@gmail.com>
2021-10-23 15:21:02 +0300

    bump

--- scripts/piot2-ctrl.sh
+++ scripts/piot2-ctrl.sh
@@ -9,36 +9,16 @@ HOOK_CLIENT="$HOOKS_DIR/piot2-client-hook.sh"
 HOOK_SERVER="$HOOKS_DIR/piot2-server-hook.sh"
 CONFIG_DIR="$SCRIPTS_DIR/cfg"
 DB_PATH="$CONFIG_DIR/piot.sqlite"
-CONTAINER_NAME_PIOT2="piot2"
-CONTAINER_NAME_GRAFANA="piot2-grafana"
-ARGS_NO_CONTAINER=""
-ARGS_DEB_PATH="./piot2*.deb"
 
 # Parse arguments
 for i in "$@"; do
     case $i in
         --action=*)
         ARGS_ACTION="${i#*=}"
-        ARGS_NO_CONTAINER+=" --action=$ARGS_ACTION"
         shift
         ;;
         --config=*)
         ARGS_CONFIG_NAME="${i#*=}"
-        ARGS_NO_CONTAINER+=" --config=$ARGS_CONFIG_NAME"
-        shift
-        ;;
-        --container)
-        ARGS_IN_CONTAINER=true
-        shift
-        ;;
-        --cmd=*)
-        ARGS_CMD="${i#*=}"
-        ARGS_NO_CONTAINER+=" --cmd=$ARGS_CMD"
-        shift
-        ;;
-        --deb-path=*)
-        ARGS_DEB_PATH="${i#*=}"
-        ARGS_NO_CONTAINER+=" --deb-path=$ARGS_DEB_PATH"
         shift
         ;;
         *)
@@ -127,54 +107,6 @@ _hook_server_apply() {
     # Apply hooks from config
     echo "Applying server hooks"
     _hook_write "$config_name" "piot2-write-sensor-to-db.sh" "$HOOK_SERVER"
-}
-
-_container_test() {
-    (which podman) &> /dev/null; rc=$?
-    if [ $rc -ne 0 ]; then
-        echo """Error: podman is missing! 
-  Install podman by running following shell command -> 
-    sudo apt-get update && sudo apt-get install podman
-
-  Or follow installation instructions here -> 
-    https://podman.io/getting-started/installation"""
-        exit 42
-    fi
-}
-
-_create_docker_file() {
-    local dest=$1
-
-    echo '''FROM ubuntu:20.04
-
-ENV container docker
-ENV LC_ALL C
-ENV DEBIAN_FRONTEND noninteractive
-
-RUN sed -i "s/# deb/deb/g" /etc/apt/sources.list
-
-RUN apt-get update \
-    && apt-get install -y systemd systemd-sysv python3 jq sqlite3 nano \
-    && apt-get clean \
-    && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
-
-RUN cd /lib/systemd/system/sysinit.target.wants/ \
-    && ls | grep -v systemd-tmpfiles-setup | xargs rm -f $1
-
-RUN rm -f /lib/systemd/system/multi-user.target.wants/* \
-    /etc/systemd/system/*.wants/* \
-    /lib/systemd/system/local-fs.target.wants/* \
-    /lib/systemd/system/sockets.target.wants/*udev* \
-    /lib/systemd/system/sockets.target.wants/*initctl* \
-    /lib/systemd/system/basic.target.wants/* \
-    /lib/systemd/system/anaconda.target.wants/* \
-    /lib/systemd/system/plymouth* \
-    /lib/systemd/system/systemd-update-utmp*
-
-VOLUME [ "/sys/fs/cgroup" ]
-
-CMD ["/lib/systemd/systemd"]
-    ''' > $dest
 }
 
 _service_enable() {
@@ -275,7 +207,6 @@ status_show() {
     json=$(echo $json | jq -Mc ".app.deployed.\"in-container-grafana\" = $tmp")
     json=$(echo $json | jq -Mc ".app.version.app = \"$APP_VERSION\"")
     json=$(echo $json | jq -Mc ".app.version.config = \"$CONFIG_VERSION\"")
-    json=$(echo $json | jq -Mc ".app.\"deb-path\" = \"$ARGS_DEB_PATH\"")
 
     # Hooks
     for hook_name in client server; do
@@ -362,150 +293,12 @@ status_show() {
     echo $json | jq
 }
 
-sensors_enable() {
-    echo "Loading sensor kernel modules"
-    sudo sh -c "echo '''# ds18b20 temperature sensor
-w1-gpio
-w1-therm
-''' > /etc/modules-load.d/piot2-sensors.conf"
-    [ $? -eq 0 ] && echo "Successfully added startup modules" \
-        || echo "Failed to add startup modules"
-    sudo modprobe w1-gpio && echo "Successfully loaded w1-gpio" \
-        || echo "Failed to load w1-gpio"
-    sudo modprobe w1-therm && echo "Successfully loaded w1-therm" \
-        || echo "Failed to load w1-therm"
-}
-
-sensors_disable() {
-    echo "Unloading sensor kernel modules"
-    sudo modprobe -r w1-therm && echo "Successfully unloaded w1-therm" \
-        || echo "Failed to unload w1-therm"
-    sudo modprobe -r w1-gpio && echo "Successfully unloaded w1-gpio" \
-        || echo "Failed to unload w1-gpio"
-    sudo rm /etc/modules-load.d/piot2-sensors.conf
-    [ $? -eq 0 ] && echo "Successfully removed startup modules" \
-        || echo "Failed to remove startup modules"
-}
-
-container_piot2_start() {
-    local name=$1
-    local dockerfile=$2
-    local rc=0
-
-    # Create image if it's absent
-    (podman image exists $name) &> /dev/null; rc=$?
-    if [ $rc -ne 0 ]; then
-        echo "Creating docker file :: path=$dockerfile"
-        _create_docker_file $dockerfile
-
-        echo "Creating image :: name=$name"
-        podman build -f $dockerfile -t $name
-
-        mnt_path="/home/$(whoami)/piot2/mnt"
-        mkdir -p $mnt_path
-
-        data_path="/home/$(whoami)/piot2/data"
-        mkdir -p $data_path
-
-        echo "Creating container mnt=$mnt_path data=$data_path"
-        podman create --name $name \
-            --volume /etc/localtime:/etc/localtime:ro \
-            --volume $mnt_path:/mnt \
-            --volume $data_path:/opt/piot2 \
-            --volume /sys/fs/cgroup:/sys/fs/cgroup:ro \
-            --tmpfs /tmp --tmpfs /run --tmpfs /run/lock \
-            $name:latest
-    fi
-
-    # Start container if it's not running
-    (podman ps --filter name=$name --filter status=running | grep $name) &> /dev/null; rc=$?
-    if [ $rc -ne 0 ]; then
-        echo "Starting container :: name=$name"
-        podman start $name
-    else
-        echo "Container is already running :: name=$name"
-    fi
-    podman ps --filter name=$name
-}
-
-container_grafana_start() {
-    local name=$1
-    local dockerfile=$2
-    local rc=0
-
-    # Create image if it's absent
-    (podman image exists $name) &> /dev/null; rc=$?
-    if [ $rc -ne 0 ]; then
-        data_path="/home/$(whoami)/piot2/data"
-        mkdir -p $data_path
-
-        echo "Creating grafana container :: path=$dockerfile"
-        podman run -d \
-            -p 3000:3000 \
-            -v $PWD:/piot2 \
-            --net="host" \
-            --name=$name \
-            -e "GF_INSTALL_PLUGINS=grafana-clock-panel,grafana-simple-json-datasource,frser-sqlite-datasource" \
-            grafana/grafana:latest-ubuntu
-    fi
-
-    # Start container if it's not running
-    (podman ps --filter name=$name --filter status=running | grep $name) &> /dev/null; rc=$?
-    if [ $rc -ne 0 ]; then
-        echo "Starting container :: name=$name"
-        podman start $name
-    else
-        echo "Container is already running :: name=$name"
-    fi
-    podman ps --filter name=$name
-}
-
-container_stop() {
-    local name=$1
-
-    echo "Stopping container :: name=$name"
-    podman stop $name
-}
-
-container_delete() {
-    local name=$1
-
-    echo "Deleting image :: name=$name"
-    podman stop $name
-    podman rm $name
-    podman rmi --force $name
-}
-
-container_status() {
-    local name=$1
-    podman ps -all --filter name=$name
-}
-
-container_shell() {
-    local name=$1
-    local cmd=$2
-
-    [ -z "$cmd" ] && cmd="/bin/bash" # Run bash by default
-    eval podman exec --interactive --tty "$name" "$cmd"
-}
-
 # ------------------------------------------------------------------------------
 # MAIN
 # ------------------------------------------------------------------------------
 main() {
     local action=$ARGS_ACTION
-    local cmd=$ARGS_CMD
     local config=$ARGS_CONFIG_NAME
-
-    # Rus action in container
-    if [ -n "$ARGS_IN_CONTAINER" ]; then
-        container_shell "$CONTAINER_NAME_PIOT2" "piot2-ctrl $ARGS_NO_CONTAINER"
-        exit $?
-    fi
-
-    # Get name of the continer we are working with now 
-    local name=`(echo $action | grep container-grafana &> /dev/null) && \
-        echo $CONTAINER_NAME_GRAFANA || echo $CONTAINER_NAME_PIOT2`
 
     # Run action locally
     case $action in
@@ -561,43 +354,6 @@ main() {
             IFS='-' read unit_name unit_status <<< $action
             [ "$unit_status" == "enable" ]  && _service_enable "$unit_name"
             [ "$unit_status" == "disable" ] && _service_disable "$unit_name"
-        ;;
-
-        # ----------------------------------------------------------------------
-        # SENSORS
-        sensors-enable|sensors-disable)
-            [ "$action" == "sensors-enable" ] && sensors_enable \
-                                              || sensors_disable
-        ;;
-
-        # ----------------------------------------------------------------------
-        # CONTAINER
-        container-start|container-grafana-start)
-            _container_test
-            dockerfile="/tmp/dockerfile.$name"
-            [ "$action" == "container-start" ]     && \
-                container_piot2_start "$name" "$dockerfile"       || 
-                container_grafana_start "$name" "$dockerfile"
-        ;;
-
-        container-stop|container-grafana-stop)
-            _container_test
-            container_stop "$name"
-        ;;
-
-        container-delete|container-grafana-delete)
-            _container_test
-            container_delete "$name"
-        ;;
-
-        container-status|container-grafana-status)
-            _container_test
-            container_status "$name"
-        ;;
-
-        container-shell|container-grafana-shell)
-            _container_test
-            container_shell "$name" "$cmd"
         ;;
 
         # ----------------------------------------------------------------------
--- scripts/piot2-installer.sh
+++ scripts/piot2-installer.sh
@@ -2,13 +2,11 @@
 
 # Vars
 PKG_NAME="piot2"
-DIR_SCRIPTS="/opt/piot2"
-DIR_HOOKS="$DIR_SCRIPTS/hooks"
 CONTAINER_PIOT2="piot2"
 CONTAINER_GRAFANA="piot2-grafana"
 
 # Parse arguments
-ARGS_ACTION="status"
+ARGS_ACTION="fuck"
 ARGS_DEB_PATH=`(ls piot2*.deb 2> /dev/null || echo piot2-not-found.deb) | head -n1`
 ARGS_CONTAINER_NAME="piot2"
 for i in "$@"; do
@@ -33,24 +31,13 @@ done
 # ------------------------------------------------------------------------------
 # PRIVATE METHODS
 # ------------------------------------------------------------------------------
-_usage () {
-    echo """
-Usage: $0 --action=xxx
-"""
-    exit 42
-}
-
 _echo_bool() {
     [ $? -eq 0 ] && echo true || echo false
 }
 
-_is_installed() {
-    [[ -d "$DIR_HOOKS" ]]
-}
-
-_container_show_status() {
+_container_list() {
     local name=$1
-    podman ps -all --filter name=$name
+    podman ps --all --filter name=piot2
 }
 
 _container_test() {
@@ -64,20 +51,24 @@ _container_test() {
 
 _container_is_running() {
     local name=$1
-    podman ps --filter name=$name --filter status=running | grep $name &> /dev/null
+    podman ps --filter name=^$name$ --filter status=running | grep $name &> /dev/null
 }
 
 _container_piot2_create() {
-    local name="$CONTAINER_PIOT2"
+    local name_container="$CONTAINER_PIOT2"
+    local name_image="localhost/$CONTAINER_PIOT2"
     local dockerfile="/tmp/$name.dockerfile"
     local rc=0
 
-    # Return if images exists
-    (podman image exists $name) &> /dev/null; rc=$?
+    # Return if container already exists
+    (podman container exists $name_container) &> /dev/null; rc=$?
     [ $rc -eq 0 ] && return
 
-    # Create dockerfile
-    echo '''FROM ubuntu:20.04
+    # Create image if it's missing
+    (podman image exists $name_image) &> /dev/null; rc=$?
+    if [ $rc -ne 0 ]; then
+        # Create dockerfile
+        echo '''FROM ubuntu:20.04
 
 ENV container docker
 ENV LC_ALL C
@@ -106,38 +97,48 @@ RUN rm -f /lib/systemd/system/multi-user.target.wants/* \
 VOLUME [ "/sys/fs/cgroup" ]
 
 CMD ["/lib/systemd/systemd"]
-    ''' > $dockerfile
+        ''' > $dockerfile
 
-    echo "Building image :: name=$name"
-    podman build -f $dockerfile -t $name
+        echo "Creating image :: name=$name_image"
+        podman build            \
+            --file $dockerfile  \
+            --tag $name_image
+    fi
 
-    echo "Creating container in $PWD"
+    echo "Creating container :: name=$name_container path=$PWD"
     mkdir -p ./mnt
-    mkdir -p ./data
-    podman create --name $name \
+    mkdir -p ./data/cfg
+    podman create \
+        --name $name_container \
+        --hostname $name_container \
         --volume /etc/localtime:/etc/localtime:ro \
+        --volume /sys/fs/cgroup:/sys/fs/cgroup:ro \
         --volume ./mnt:/mnt \
         --volume ./data:/opt/piot2 \
-        --volume /sys/fs/cgroup:/sys/fs/cgroup:ro \
-        --tmpfs /tmp --tmpfs /run --tmpfs /run/lock \
-        $name:latest
+        --tmpfs /tmp \
+        --tmpfs /run \
+        --tmpfs /run/lock \
+        $name_image
 }
 
 _container_grafana_create() {
-    local name="$CONTAINER_GRAFANA"
+    local name_container="$CONTAINER_GRAFANA"
+    local name_image="grafana/grafana:latest-ubuntu"
     local rc=0
 
     # Return if images exists
-    (podman image exists $name) &> /dev/null; rc=$?
+    (podman container exists $name_container) &> /dev/null; rc=$?
     [ $rc -eq 0 ] && return
 
-    echo "Creating grafana container :: name=$name"
+    echo "Creating container :: name=$name_container path=$PWD"
     podman run -d \
-        --volume $PWD/data/cfg:/piot2 \
+        -p 3000:3000 \
+        --name=$name_container \
+        --hostname=$name_container \
+        --volume ./data/cfg:/piot2 \
         --net="host" \
-        --name=$name \
         -e "GF_INSTALL_PLUGINS=grafana-clock-panel,grafana-simple-json-datasource,frser-sqlite-datasource" \
-        grafana/grafana:latest-ubuntu
+        $name_image
 }
 
 # ------------------------------------------------------------------------------
@@ -166,7 +167,6 @@ container_start() {
     else
         echo "Container is already running :: name=$name"
     fi
-    podman ps --filter name=$name
 }
 
 container_stop() {
@@ -179,7 +179,14 @@ container_stop() {
         echo "Stopping container :: name=$name"
         podman stop $name
     fi
-    podman ps --filter name=$name
+}
+
+container_delete() {
+    local name=$1
+
+    echo "Deleting container :: name=$name"
+    podman stop $name > /dev/null 2>&1
+    podman rm $name
 }
 
 container_install_deb() {
@@ -192,34 +199,102 @@ container_install_deb() {
         container_shell $ARGS_CONTAINER_NAME "dpkg -i /mnt/$deb_name"
 }
 
+sensors_enable() {
+    echo "Loading sensor kernel modules"
+    sudo sh -c "echo '''# ds18b20 temperature sensor
+w1-gpio
+w1-therm
+''' > /etc/modules-load.d/piot2-sensors.conf"
+    [ $? -eq 0 ] && echo "Successfully added startup modules" \
+        || echo "Failed to add startup modules"
+    sudo modprobe w1-gpio && echo "Successfully loaded w1-gpio" \
+        || echo "Failed to load w1-gpio"
+    sudo modprobe w1-therm && echo "Successfully loaded w1-therm" \
+        || echo "Failed to load w1-therm"
+}
+
+sensors_disable() {
+    echo "Unloading sensor kernel modules"
+    sudo modprobe -r w1-therm && echo "Successfully unloaded w1-therm" \
+        || echo "Failed to unload w1-therm"
+    sudo modprobe -r w1-gpio && echo "Successfully unloaded w1-gpio" \
+        || echo "Failed to unload w1-gpio"
+    sudo rm /etc/modules-load.d/piot2-sensors.conf
+    [ $? -eq 0 ] && echo "Successfully removed startup modules" \
+        || echo "Failed to remove startup modules"
+}
+
 status_show() {
     local json="{}"
+    local tmp=""
+    local ccc=""
+    local prefix=""
 
-    # container.piot2.is-installed
-    tmp=`podman container exists $CONTAINER_PIOT2 &> /dev/null; _echo_bool`
-    json=$(echo $json | jq -Mc ".\"$CONTAINER_PIOT2\".\"is-installed\" = $tmp")
+    # containers.piot2.is-installed
+    ccc=$CONTAINER_PIOT2
+    prefix=".containers.\"$ccc\""
+    tmp=`podman container exists $ccc > /dev/null 2>&1; _echo_bool`
+    json=$(echo $json | jq -Mc "$prefix.\"is-installed\" = $tmp")
 
-    # container.piot2.is-running
-    tmp=`_container_is_running $CONTAINER_PIOT2; _echo_bool`
-    json=$(echo $json | jq -Mc ".\"$CONTAINER_PIOT2\".\"is-running\" = $tmp")
+    # containers.piot2.is-running
+    tmp=`_container_is_running $ccc; _echo_bool`
+    json=$(echo $json | jq -Mc "$prefix.\"is-running\" = $tmp")
 
-    # container.piot2.deb-version
+    # containers.piot2.deb-version
     cmd="dpkg -S $PKG_NAME > /dev/null 2>&1 && dpkg-query --show $PKG_NAME | cut -f2 || echo null"
-    tmp=`_container_is_running $CONTAINER_PIOT2 && \
-        container_shell $CONTAINER_PIOT2 "$cmd"`
+    tmp=`_container_is_running $ccc && \
+        container_shell $ccc "$cmd" || echo null`
     [ "$tmp" != "null" ] && tmp="\"$tmp\""
-    json=$(echo $json | jq -Mc ".\"$CONTAINER_PIOT2\".\"deb-version\" = $tmp")
+    json=$(echo $json | jq -Mc "$prefix.\"deb-version\" = $tmp")
 
-    # container.piot2-grafana.is-installed
-    tmp=`podman container exists $CONTAINER_GRAFANA &> /dev/null; _echo_bool`
-    json=$(echo $json | jq -Mc ".\"$CONTAINER_GRAFANA\".\"is-installed\" = $tmp")
+    # containers.piot2-grafana.is-installed
+    ccc=$CONTAINER_GRAFANA
+    prefix=".containers.\"$ccc\""
+    tmp=`podman container exists $ccc > /dev/null 2>&1; _echo_bool`
+    json=$(echo $json | jq -Mc "$prefix.\"is-installed\" = $tmp")
 
     # container.piot2-grafana.is-running
-    tmp=`_container_is_running $CONTAINER_GRAFANA; _echo_bool`
-    json=$(echo $json | jq -Mc ".\"$CONTAINER_GRAFANA\".\"is-running\" = $tmp")
+    tmp=`_container_is_running $ccc; _echo_bool`
+    json=$(echo $json | jq -Mc "$prefix.\"is-running\" = $tmp")
+
+    # sensors.w1-gpio1
+    tmp=`grep -e "^w1_gpio " /proc/modules > /dev/null 2>&1; _echo_bool`
+    json=$(echo $json | jq -Mc ".sensors.\"w1-gpio\" = $tmp")
+
+    # sensors.w1-therm
+    tmp=`grep -e "^w1_therm " /proc/modules > /dev/null 2>&1; _echo_bool`
+    json=$(echo $json | jq -Mc ".sensors.\"w1-therm\" = $tmp")
 
     # Dump
     echo $json | jq
+}
+
+usage () {
+    echo """
+PIOT2 Installer: 
+  Status:
+    $0 --action=status
+
+  Sensors:
+    $0 --action=sensors-enable
+    $0 --action=sensors-disable
+
+  Container piot2:
+    $0 --action=create
+    $0 --action=start
+    $0 --action=stop
+    $0 --action=delete
+    $0 --action=install-deb --deb-path=~/git/piot2_0.1.0_all.deb
+    $0 --action=shell
+
+  Container piot2-grafana:
+    $0 --action=create --container=piot2-grafana 
+    $0 --action=start --container=piot2-grafana
+    $0 --action=stop --container=piot2-grafana
+    $0 --action=delete --container=piot2-grafana
+    $0 --action=shell --container=piot2-grafana
+"""
+    exit 42
 }
 
 # ------------------------------------------------------------------------------
@@ -229,36 +304,57 @@ main() {
     local action=$ARGS_ACTION
     local container=$ARGS_CONTAINER_NAME
     local deb_path=$ARGS_DEB_PATH
+    local list_running_containers=true
 
-    # Run action locally
+    # Run action
     case $action in
         shell)
             _container_test
             container_shell "$container"
+            list_running_containers=false
         ;;
-
-        start)
+        create)
             _container_test
             [ "$container" == "$CONTAINER_PIOT2" ] && \
                 _container_piot2_create || \
                 _container_grafana_create
-
+        ;;
+        start)
+            _container_test
             container_start "$container"
         ;;
-
         stop)
             _container_test
             container_stop "$container"
         ;;
-
+        delete)
+            _container_test
+            container_delete "$container"
+        ;;
         install-deb)
             _container_test
             container_install_deb "$container" "$deb_path"
         ;;
-
-        *|status)
+        sensors-enable)
+            sensors_enable
+            list_running_containers=false
+        ;;
+        sensors-disable)
+            sensors_disable
+            list_running_containers=false
+        ;;
+        status)
             status_show
         ;;
+        *)
+            usage
+            list_running_containers=false
+        ;;
     esac
+
+    # List runnng containers
+    [ "$list_running_containers" = true ]           \
+        && echo                                     \
+        && podman ps --all --filter name=piot2
 }
 main
